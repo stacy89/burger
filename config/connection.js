6443443afa2c13f7a24dbad03ad81d0a1b4162fa
Stacy Areas <stacyareas@gmail.com>
2018-02-28 16:50:11 -0800

    create database -- create connection for mysql

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+	port: 3306,
+	host: "",
+	user: "root",
+	password: "burgers_db"
+});
+
+connection.connect(function(err) {
+	if (err) {
+		console.log("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
