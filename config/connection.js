8a0b1ac3d184e91303e805e60f6b6e698a194920
Stacy Areas <stacyareas@gmail.com>
2018-03-02 19:53:34 -0800

    add jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-	port: 3306,
-	host: "127.0.0.1",
-	user: "root",
-	password: "", 
-	database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL) 
+} else {
+	connection = mysql.createConnection({
+		port: 3306,
+		host: "127.0.0.1",
+		user: "root",
+		password: "", 
+		database: "burgers_db"
+	});
+}
 
 connection.connect(function(err) {
 	if (err) {
