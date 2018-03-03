803a73e65917e560ac85b4dce0851b0d1b1de2d8
BUMBALORD <davidhallinan@gmail.com>
2018-03-02 18:14:04 -0800

    tiniest lil change in the world

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ var port = process.env.PORT || 8000;
 var app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controllers.js");
